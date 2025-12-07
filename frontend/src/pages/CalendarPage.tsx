ce89b776359e9794bff3e4d4d397c942535f9fda
Amelie Engelmaierová <enga03@vse.cz>
2025-12-07 18:06:56 +0100

    fix(pipeline): fixed, gitlab-ci-local

--- frontend/src/pages/CalendarPage.tsx
+++ frontend/src/pages/CalendarPage.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { useState } from 'react'
+import { useState } from 'react'
 import { useCalendarApp, ScheduleXCalendar } from '@schedule-x/react'
 import {
   createViewDay,
