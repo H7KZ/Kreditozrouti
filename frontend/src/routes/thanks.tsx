34e40ec228043cce0d708bc9aa4655a93803e2c2
Amelie Engelmaierová <enga03@vse.cz>
2025-12-07 17:47:13 +0100

    fix(label): pipeline interface issue

--- frontend/src/routes/thanks.tsx
+++
@@ -1,9 +0,0 @@
-import { createRoute } from '@tanstack/react-router'
-import { Route as RootRoute } from './__root'
-import ThanksPage from '@/pages/ThanksPage'
-
-export const Route = createRoute({
-  getParentRoute: () => RootRoute,
-  path: '/thankssss',
-  component: ThanksPage,
-})
