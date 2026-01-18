75b8c498a4718907c5740861c1961766737a3126
Amelie Engelmaierová <amelie.engelmaier@gmail.com>
2026-01-18 23:41:51 +0100

    feat: added basic calendar setup

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
 import Alerts from '@client/components/alert/Alerts.vue'
+import Scheduler from '@client/components/scheduler/Scheduler.vue'
 </script>
 
 <template>
 	<main>
 		<RouterView />
-
+			<Scheduler />
 		<Alerts />
 	</main>
 </template>
