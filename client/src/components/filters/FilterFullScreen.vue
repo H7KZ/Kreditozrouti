dc44012852b104d88e25756044a3659e19c61008
H7KZ <74021016+H7KZ@users.noreply.github.com>
2026-06-21 16:19:20 +0200

    feat(client): add FilterFullScreen wrapper component

---
+++ client/src/components/filters/FilterFullScreen.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import FilterPanel from '@client/components/filters/FilterPanel.vue'
+</script>
+
+<template>
+  <div class="fixed inset-0 z-50 flex flex-col pt-[env(safe-area-inset-top)]">
+    <FilterPanel class="flex-1 min-h-0" />
+  </div>
+</template>
