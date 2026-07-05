ada351fdc84e39afd0ac90a6346eb681ace83785
H7KZ <74021016+H7KZ@users.noreply.github.com>
2026-07-05 14:17:09 +0200

    chore: scaffold packages/core (@kreditozrouti/core)

---
+++ packages/core/tsup.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'tsup'
+
+export default defineConfig({
+  entry: {
+    'domain/index': 'src/domain/index.ts',
+    'db/index': 'src/db/index.ts',
+    'services/index': 'src/services/index.ts',
+    'http/index': 'src/http/index.ts',
+    'queue/index': 'src/queue/index.ts',
+    'utils/index': 'src/utils/index.ts',
+  },
+  format: ['esm'],
+  dts: true,
+  clean: true,
+  sourcemap: true,
+})
