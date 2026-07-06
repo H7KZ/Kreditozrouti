9611bb28194971cd151ad3c7b0ab2869a45638bf
H7KZ <74021016+H7KZ@users.noreply.github.com>
2026-07-06 23:31:47 +0200

    feat(docs): remove topbar, replace with navbar CTA pill button

--- docs/.vitepress/theme/Layout.vue
+++ docs/.vitepress/theme/Layout.vue
@@ -5,11 +5,5 @@ const { Layout: VPLayout } = DefaultTheme
 </script>
 
 <template>
-  <div class="docs-shell">
-    <div class="docs-topbar">
-      <a href="https://kreditozrouti.cz" class="docs-back-link">← Open Kreditožrouti</a>
-      <span class="docs-topbar-name">Kreditožrouti Docs</span>
-    </div>
-    <VPLayout />
-  </div>
+	<VPLayout />
 </template>
